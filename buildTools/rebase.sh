db76294231b9ea095a846514aacc8db70d7f402d
dangherve <dangherve@gmail.com>
2026-03-12 13:42:18 -0400

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git remote add upstream https://github.com/librespeed/speedtest.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/speedtest.git
+git push --force --set-upstream origin master
